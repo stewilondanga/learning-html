cb6213728855a2ae544414adfa47aedc38a6a17a
Stewart ilondanga <stilondanga@gmail.com>
2018-08-08 14:38:43 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
+
+Array.prototype.forEach.call(document.querySelectorAll('.key li'), function(li) {
+  li.addEventListener('mouseenter', function() {
+    document.body.classList.add('hover-' + this.className);
+  });
+  li.addEventListener('mouseleave', function() {
+    document.body.classList.remove('hover-' + this.className);
+  });
+});
